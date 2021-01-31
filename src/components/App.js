c5d1d0ed9cd6411c6d989d0af717f49a27103f8d
scbonner <45452011+scbonner@users.noreply.github.com>
2021-01-31 17:09:12 -0500

    the form looks picture perfect

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import React from 'react'
-// import Signup from './Signup'
-// import { Container } from 'react-bootstrap'
+import Signup from './Signup'
+import { Container } from 'react-bootstrap'
 // import { AuthProvider } from '../contexts/AuthContext'
 
 
 function App() {
-   return "Hello World I hope this works"
+   return (
 //     <AuthProvider>
-//       <Container 
-//         className='d-flex align-items-center justify-content-center'
-//         style={{ minHeight: '100vh' }}
-//        >
-//         <div className='w-100' style={{ maxWidth: '400px' }}>
-//           <Signup />
-//         </div>
-//       </Container>
+    <Container 
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: '100vh' }}
+       >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Signup />
+        </div>
+      </Container>
 //     </AuthProvider>
-//   ) 
+   ) 
  }
 
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,12 +1,12 @@
-// import React, { useRef, useStateS } from 'react'
-// import { Form, Button, Card, Alert } from 'react-bootstrap'
+import React, { useRef } from 'react'
+import { Form, Button, Card } from 'react-bootstrap'
 // import { useAuth } from '../contexts/AuthContext'
 
 
-// export default function Signup() {
-//      const emailRef = useRef()
-//      const passwordRef = useRef()
-//      const passwordConfirmRef = useRef()
+export default function Signup() {
+     const emailRef = useRef()
+     const passwordRef = useRef()
+     const passwordConfirmRef = useRef()
 //      const { signup } = useAuth()
 //      const [error, setError] = useState("")
 //      const [loading, setLoading] = useState(false)  //set state because we are not loadin it is set to false
@@ -30,32 +30,32 @@
 
 //      setLoading(false)
 //     }   
-//     return (
-//         <>
-//             <Card>
-//                 <Card.Body>
-//                     <h2 className='text-center mb-4'>Sign Up</h2>
-//                     {error && <Alert variant='danger'>{error}</Alert>}
-//                         <Form onSubmit="handleSubmit">
-//                         <Form.Group id='email'>
-//                             <Form.Label>Email</Form.Label>
-//                             <Form.Control type='email' ref={emailRef} required />
-//                         </Form.Group>
-//                         <Form.Group id='password'>
-//                             <Form.Label>Password</Form.Label>
-//                             <Form.Control type='password' ref={passwordRef} required />
-//                         </Form.Group>
-//                         <Form.Group id='confirm-password'>
-//                             <Form.Label>Password Confirmation</Form.Label>
-//                             <Form.Control type='password' ref={passwordConfirmRef} required />
-//                         </Form.Group>
-//                         <Button disabled={loading} className='w-100' type='submit'>Sign Up</Button>
-//                         </Form>
-//                 </Card.Body>
-//             </Card>
-//             <div className='w-100 text-center mt-2'>
-//                 Already have an account? Log In
-//             </div>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Sign Up</h2>
+                    {/* {error && <Alert variant='danger'>{error}</Alert>} */}
+                        <Form onSubmit="handleSubmit">
+                        <Form.Group id='email'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type='email' ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id='password'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' ref={passwordRef} required />
+                        </Form.Group>
+                        <Form.Group id='confirm-password'>
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type='password' ref={passwordConfirmRef} required />
+                        </Form.Group>
+                        <Button  className='w-100' type='submit'>Sign Up</Button>
+                        </Form>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                Already have an account? Log In
+            </div>
+        </>
+    )
+}

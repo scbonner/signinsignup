34f8762bcd870ea40050a486fd192071f39bce23
scbonner <45452011+scbonner@users.noreply.github.com>
2021-01-31 16:55:54 -0500

    having to rework restart code breaking

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'Firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/auth'
 
 // development and production environments in placed to access
@@ -11,38 +11,7 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-
 })
 // function that gives us our authentication and firebase uses
 export const auth = app.auth()
 export default app
-
-// Options to configure the app's services.
-
-
-// Creates and initializes a Firebase {@link firebase.app.App app} instance.
-
-// See {@link https://firebase.google.com/docs/web/setup#add_firebase_to_your_app Add Firebase to your app} and {@link https://firebase.google.com/docs/web/setup#multiple-projects Initialize multiple projects} for detailed documentation.
-
-// @example
-
-
-// // Initialize default app
-// // Retrieve your own options values by adding a web app on
-// // https://console.firebase.google.com
-// firebase.initializeApp({
-//   apiKey: "AIza....",                             // Auth / General Use
-//   appId: "1:27992087142:web:ce....",      // General Use
-//   projectId: "my-firebase-project",               // General Use
-//   authDomain: "YOUR_APP.firebaseapp.com",         // Auth with popup/redirect
-//   databaseURL: "https://YOUR_APP.firebaseio.com", // Realtime Database
-//   storageBucket: "YOUR_APP.appspot.com",          // Storage
-//   messagingSenderId: "123456789",                  // Cloud Messaging
-//   measurementId: "G-12345"                        // Analytics
-// });
-// @example
-
-
-// // Initialize another app
-// var otherApp = firebase.initializeApp({
-//   apiKey: "AIza....",
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,36 +1,61 @@
-import React from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+// import React, { useRef, useStateS } from 'react'
+// import { Form, Button, Card, Alert } from 'react-bootstrap'
+// import { useAuth } from '../contexts/AuthContext'
 
 
-const Signup = () => {
-    return (
-// adding fragment because of Card 
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className='text-center mb-4'>Sign Up</h2>
-                    <Form>
-                      <Form.Group id='email'>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' ref={emailRef} required />
-                      </Form.Group>
-                      <Form.Group id='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' ref={passwordRef} required />
-                      </Form.Group>
-                      <Form.Group id='confirm-password'>
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type='password' ref={passwordConfirmRef} required />
-                      </Form.Group>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className='w-100 text-center mt-2'>
-                Already have an account? Log In
-            </div>
-            
-        </>
-    )
-}
+// export default function Signup() {
+//      const emailRef = useRef()
+//      const passwordRef = useRef()
+//      const passwordConfirmRef = useRef()
+//      const { signup } = useAuth()
+//      const [error, setError] = useState("")
+//      const [loading, setLoading] = useState(false)  //set state because we are not loadin it is set to false
 
-export default Signup
+//      async function handleSubmit(e) {
+//          //prevents screen form from refreshing
+//          e.preventDefault()
+    
+//          if (passwordRef.current.value !== 
+//          passwordConfirmRef.current.value) {
+//              return setError('Passwords do not match')
+//     }
+
+//      try {
+//          setError("")
+//          setLoading(true)
+//           signup(emailRef.current.value, passwordRef.current.value)
+//      }   catch {
+//          setError('Failed to create account')
+//      }
+
+//      setLoading(false)
+//     }   
+//     return (
+//         <>
+//             <Card>
+//                 <Card.Body>
+//                     <h2 className='text-center mb-4'>Sign Up</h2>
+//                     {error && <Alert variant='danger'>{error}</Alert>}
+//                         <Form onSubmit="handleSubmit">
+//                         <Form.Group id='email'>
+//                             <Form.Label>Email</Form.Label>
+//                             <Form.Control type='email' ref={emailRef} required />
+//                         </Form.Group>
+//                         <Form.Group id='password'>
+//                             <Form.Label>Password</Form.Label>
+//                             <Form.Control type='password' ref={passwordRef} required />
+//                         </Form.Group>
+//                         <Form.Group id='confirm-password'>
+//                             <Form.Label>Password Confirmation</Form.Label>
+//                             <Form.Control type='password' ref={passwordConfirmRef} required />
+//                         </Form.Group>
+//                         <Button disabled={loading} className='w-100' type='submit'>Sign Up</Button>
+//                         </Form>
+//                 </Card.Body>
+//             </Card>
+//             <div className='w-100 text-center mt-2'>
+//                 Already have an account? Log In
+//             </div>
+//         </>
+//     )
+// }
